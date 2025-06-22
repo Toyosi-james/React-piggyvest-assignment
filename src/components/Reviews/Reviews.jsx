8bc049dfd70f2e7795611b35a5c98b9256ea6729
Toyosi-james <Jamestoyosi84@gmail.com>
2025-06-22 18:22:12 +0100

    react piggvest assignment

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,5 @@
 import "./Reviews.css"
-import fallback from "../../assets/fallbackimg.png"
+import fallback from "../../assets/fallbacklmg.png"
 import img1 from "../../assets/img1.jpeg"
 import img2 from "../../assets/img2.jpeg"
 import img3 from "../../assets/img3.jpeg"
